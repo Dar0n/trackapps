832f9a68084bbc3fbc337716f036a9211e740034
Ilya <shatuncheg@gmail.com>
2018-07-24 11:28:45 +0200

    Add views for GetUpdateDelete jobs and list all jobs

--- backend/project/api/jobs/urls.py
+++ backend/project/api/jobs/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from api.jobs.views import PostNewJob
+from api.jobs.views import PostNewJobView, GetUpdateDeleteJobView, GetListOfJobsView
 
 app_name = 'jobs'
 
 urlpatterns = [
-    path('newjob/', PostNewJob.as_view(), name='new-job'),
+    path('newjob/', PostNewJobView.as_view(), name='new-job'),
+    path('job/<int:job_id>/', GetUpdateDeleteJobView.as_view(), name='get-update-delete-jobs'),
+    path('listjobs/', GetListOfJobsView.as_view(), name='get-all-jobs'),
 ]
--- backend/project/api/jobs/views.py
+++ backend/project/api/jobs/views.py
@@ -1,11 +1,13 @@
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.jobs.serializers import JobSerializer
+from project.jobs.models import Job
 
 
-class PostNewJob(APIView):
+class PostNewJobView(APIView):
     permission_classes = [
         IsAuthenticated,
     ]
@@ -18,3 +20,37 @@ class PostNewJob(APIView):
         serializer.is_valid(raise_exception=True)
         job = serializer.create(serializer.validated_data)
         return Response(JobSerializer(job).data)
+
+class GetUpdateDeleteJobView(GenericAPIView):
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get(self, request, job_id):
+        job = Job.objects.get(id=job_id)
+        serializer = JobSerializer(job)
+        return Response(serializer.data)
+
+    def post(self, request, job_id):
+        job = Job.objects.get(id=job_id)
+        # self.check_object_permissions(request, review)
+        serializer = JobSerializer(job, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, job_id):
+        review = Job.objects.get(id=job_id)
+        # self.check_object_permissions(request, review)
+        review.delete()
+        return Response("OK")
+
+class GetListOfJobsView(APIView):
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get(self, request):
+        jobs = Job.objects.filter(user_profile=request.user.user_profile)
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data)
