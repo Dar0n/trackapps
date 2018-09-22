794f5902d4a1e318e6ed86381ad351d08bf893bc
Ilya <shatuncheg@gmail.com>
2018-09-22 22:05:24 +0200

    Basic example with react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,15 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>, document.getElementById('root'));
