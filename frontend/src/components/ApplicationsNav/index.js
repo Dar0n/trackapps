3b2dab996f910387d1560203d7d4912999b2819d
Ilya <shatuncheg@gmail.com>
2018-09-26 11:59:15 +0200

    Make button "New application" to display conditionally only for logged in users.

--- frontend/src/components/ApplicationsNav/index.js
+++ frontend/src/components/ApplicationsNav/index.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { checkIfLoggedIn } from '../../helpers/checkIfLoggedIn';
 import './style.css';
 
 export default class ApplicationsNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logged_in: false,
+    }
+  }
+
+  async componentDidMount() {
+    const logged_in = await checkIfLoggedIn();
+    this.setState({logged_in});
+  }
+
   render() {
+    let button = '';
+    if (this.state.logged_in) {
+      button = <Link to='/new-application' className='applications__navbutton applications__navbutton--new-application'>New application</Link>
+    }
     return (
       <nav className='applications-nav'>
         <div className='applications-navbuttons-container'>
           <div className='applications__navbutton'>Calendar view</div>
           <div className='applications__navbutton'>Display: all</div>
-          <Link to='/new-application' className='applications__navbutton applications__navbutton--new-application'>New application</Link>
+          {button}
         </div>
         <div className='applications__searchline-container'>
           <input className='applications__searchline' type='text' placeholder='search...'/>
